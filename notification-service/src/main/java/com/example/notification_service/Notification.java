521216365e5505283914aa78f0634a983d32833a
leticia.blom <leticia.blom@dtidigital.com.br>
2024-10-23 11:59:57 -0300

    parte 1

---
+++ notification-service/src/main/java/com/example/notification_service/Notification.java
@@ -0,0 +1,30 @@
+package com.example.notification_service;
+
+public class Notification {
+    private int id;
+    private String message;
+
+    public Notification() {
+    }
+
+    public Notification(int id, String message) {
+        this.id = id;
+        this.message = message;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
